3477298b70c316e42fef5f8e04b96c8e1c2f3e2a
. Gameplay <04astapov04@gmail.com>
2026-05-24 20:46:07 +0700

    Simple Tasks almost DONE

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -52,6 +52,59 @@ namespace DataTypes
             Console.WriteLine($"Size: {sizeof(ushort)}");
             Console.WriteLine($"Range: {ushort.MinValue} ... {ushort.MaxValue}");
             Console.WriteLine(((ushort)0).GetType());
+
+            // HW
+
+            Console.WriteLine(delimiter);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("int");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(int)}");
+            Console.WriteLine($"Range: {int.MinValue} ... {int.MaxValue}");
+            Console.WriteLine(((int)0).GetType());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("uint");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(uint)}");
+            Console.WriteLine($"Range: {uint.MinValue} ... {uint.MaxValue}");
+            Console.WriteLine(((uint)0).GetType());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("long");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(long)}");
+            Console.WriteLine($"Range: {long.MinValue} ... {long.MaxValue}");
+            Console.WriteLine(((long)0).GetType());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("ulong");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(ulong)}");
+            Console.WriteLine($"Range: {ulong.MinValue} ... {ulong.MaxValue}");
+            Console.WriteLine(((ulong)0).GetType());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("float");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(float)}");
+            Console.WriteLine($"Range: {float.MinValue} ... {float.MaxValue}");
+            Console.WriteLine(((float)0).GetType());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("double");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(double)}");
+            Console.WriteLine($"Range: {double.MinValue} ... {double.MaxValue}");
+            Console.WriteLine(((double)0).GetType());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("decimal");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(decimal)}");
+            Console.WriteLine($"Range: {decimal.MinValue} ... {decimal.MaxValue}");
+            Console.WriteLine(((decimal)0).GetType());
         }
     }
 }
---
+++ SimpleTasks/Program.cs
@@ -0,0 +1,61 @@
+﻿//#define FACTORIAL
+//#define POWER
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if FACTORIAL
+            Console.WriteLine("Введите число для вычисления факториала: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(result); 
+#endif
+
+#if POWER
+            Console.WriteLine("Введите число для возведения в степень: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень: ");
+            int power = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Pow(num, power)); 
+#endif
+
+            Console.WriteLine("Введите лимит ряда фибоначи: ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 0; i <= limit; i++)
+            {
+                if (limit <= 0)
+                {
+                    Console.WriteLine(0);
+                    break;
+                }
+
+                if (i == 0)
+                {
+                    Console.WriteLine(0);
+                    Console.WriteLine(1);
+                    continue;
+                }
+                result = result + i - 1;
+                if (result <= limit)Console.WriteLine(result);
+
+            }
+        }
+    }
+}
